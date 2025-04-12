c1b826bd2a51bde8d140d7c7a9dd21dfa92d5bf7
Rajdipsinh Solanki <rajdipsolanki0000@gmail.com>
2025-04-12 13:28:20 +0530

    some changes in bottom navigation

--- lib/screens/bottom_nav.dart
+++ lib/screens/bottom_nav.dart
@@ -3,6 +3,9 @@ import 'package:HRMS/employee_management/apply_leave_screen.dart';
 import 'package:HRMS/employee_management/employee_home_screen.dart';
 import 'package:HRMS/employee_management/notification_management_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/theme_provider.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -17,38 +20,45 @@ class _BottomNavState extends State<BottomNav> {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final isDarkMode = themeProvider.themeMode == ThemeMode.dark;
+
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          unselectedItemColor: Colors.black,
-          type: BottomNavigationBarType.fixed,
-          onTap: (index) {
-            setState(() {
-              myIndex = index;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home_filled,color: Colors.black,),
-                label: 'home',
-                activeIcon: Icon(Icons.home_outlined,color: Colors.black,)
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person_rounded,color: Colors.black,),
-                label: 'Attendance',
-                activeIcon: Icon(Icons.person_outline,color: Colors.black,)
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.time_to_leave),
-              label: 'Apply Leave',
-              activeIcon:Image.asset('assets/video.png',height: 25,width: 25,fit: BoxFit.cover,),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.notifications_active),
-              label: 'Notifications',
-              activeIcon:Icon(Icons.notifications_on_outlined),
-            ),
-          ]),
+        currentIndex: myIndex,
+        backgroundColor: isDarkMode ? Colors.grey[700] : Colors.white,
+        selectedItemColor: isDarkMode ? Colors.white : Colors.blue,
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() {
+            myIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home_rounded),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner_outlined),
+            activeIcon: Icon(Icons.qr_code_scanner),
+            label: 'Attendance',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.time_to_leave_outlined),
+            activeIcon: Icon(Icons.time_to_leave_rounded),
+            label: 'Apply Leave',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_none),
+            activeIcon: Icon(Icons.notifications_active),
+            label: 'Notifications',
+          ),
+        ],
+      ),
+
       body: Center(
         child: widgetList[myIndex],
       ),
